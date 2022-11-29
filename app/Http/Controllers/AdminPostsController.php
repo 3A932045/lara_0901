bec61e4c3b24594066c513989a2dadb14df8cc69
3A932045 <shadowy999@gmail.com>
2022-11-29 23:24:41 +0800

    基本環境建構

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'DESC')->get();
+
+        $data = [
+            'posts'=>$posts,
+        ];
+
+        return view('admin.posts.index',$data);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        /*Post::create([
+            'title' => '',
+            'content' => '',
+        ]);*/
+
+        //dump $ die
+        //dd($request->all());
+
+        Post::create($request->all());
+
+        return redirect()->route('admin.posts.index');
+    }
+    public function edit(Post $post)
+    {
+        $data = [
+            'post' => $post,
+        ];
+
+        return view('admin.posts.edit',$data);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $post->update($request->all());
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
