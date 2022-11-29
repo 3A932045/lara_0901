c1bf7cc7f83f9c29557d12013eba5d3306050dbf
3A932045 <shadowy999@gmail.com>
2022-11-29 23:49:56 +0800

    create新增文章

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,18 +15,24 @@
         </ul>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-    <form action="{{route('admin.posts.store')}}" method="POST">
-        @method('POST')
+    <form action="{{route('admin.posts.store')}}" method="POST" role="form">
         @csrf
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">文章標題</label>
-            <input id="title" name="title" type="text" class="form-control" placeholder="請輸入文章標題">
+        <div class="form-group">
+            <label for="title" class="form-label">標題：</label>
+            <input id="title" name="title" class="form-control" value="{{old('title')}}" placeholder="請輸入文章標題">
         </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">文章內容</label>
-            <textarea id="content" name="content" class="form-control" rows="10" placeholder="請輸入文章內容"></textarea>
+        <div class="form-group">
+            <label for="content" class="form-label">內容：</label>
+            <textarea id="content" name="content" class="form-control" rows="10">{{old('content')}}</textarea>
         </div>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <div class="form-group">
+            <label for="is_feature" class="form-label">精選？</label>
+            <select id="is_feature" name="is_feature" class="form-control">
+                <option value="0">否</option>
+                <option value="1">是</option>
+            </select>
+        </div>
+        <div class="text-right">
             <button class="btn btn-primary btn-sm" type="submit">儲存</button>
         </div>
     </form>
